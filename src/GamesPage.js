4eed90729a0d971df5cef38bb73aaa12b395cb3e
Josiah Maina <josiah5115@gmail.com>
2018-09-09 17:12:30 -0600

    fixed propTypes bug

--- src/GamesPage.js
+++ src/GamesPage.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import GamesList from './GamesList';
 
 class GamesPage extends React.Component {
  render() {
   return (
    <div>
     <h1>Games List</h1>
+
+    <GamesList games={this.props.games} />
    </div>
   )
  }
 };
 
-export default GamesPage;
+
+GamesPage.protoTypes = {
+ games: PropTypes.array.isRequired
+}
+
+// GamesPage.propTypes = {
+ // games: PropTypes.array.isRequired
+ // games: PropTypes.arrayOf(PropTypes.shape({
+ //  name: PropTypes.string,
+ //  rating: PropTypes.number,
+ // })).isRequired
+// };
+
+function mapStateToProps(state) {
+ return {
+  games: state.games
+ }
+};
+
+export default connect(mapStateToProps)(GamesPage);
