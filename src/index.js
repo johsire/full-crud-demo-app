7e3ec1c469d26e30989866649eac6de029db3856
Josiah Maina <josiah5115@gmail.com>
2018-09-09 14:22:19 -0600

    redux store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+
+import { createStore } from 'redux';
+import rootReducer from './rootReducer';
+
+const store = createStore(
+ rootReducer
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
